3e3e25a6fca2719880ae49c837d71671f69b4683
gabriellamiesner <ella.miesner@gmail.com>
2022-10-04 11:49:56 -0500

    state - lines not completely reading

--- test.py
+++ test.py
@@ -4,22 +4,27 @@ import pandas as pd
 from pyparsing import And
 
 
-data = pd.read_csv('chad_rose_missouri.csv')
+data = pd.read_csv('chad.csv' ,error_bad_lines=False)
 data.loc[:, "Reach"] = data["Reach"].round(-3).map('{:,d}'.format)
 state_placements = []
 national_placements = []
 international_placements = []
 
+
+print(data)
 count = 0
 for i in data: 
     if data['Country'][count] != "United States":
         international_placements.append({"date": data['Alternate Date Format'][count], "source": data["Source"][count],  "url": data["URL"][count], "country": data["Country"][count], "reach": data["Reach"][count]})
-    elif data['State'][count] != "Missouri":
+    if data['State'][count] != "Missouri":
         national_placements.append({"date": data['Alternate Date Format'][count], "source": data["Source"][count], "url": data["URL"][count], "state": data["State"][count], "reach": data["Reach"][count]})
-    elif data['State'][count] == "Missouri": 
+    else: 
         state_placements.append({"date": data['Alternate Date Format'][count], "source": data["Source"][count], "url": data["URL"][count], "city": data["City"][count], "reach": data["Reach"][count]})
-    count += 1 
-print(state_placements)
+    # if data['Country'][count] == "United States": 
+    #     print(data["State"][count])
+    # print(data["State"][count])
+    count += 1
+# print(state_placements)
 
 # Instantiation of inherited class
 pdf = FPDF()
@@ -37,8 +42,8 @@ pdf.set_font('Times', 'B', 12)
 pdf.set_text_color(r = 0, g = 0, b = 0 )
 pdf.cell(0, 10, "State Placements", 0, 1)
 pdf.set_font('Times', '', 12)
-for i in range(1, 41):
-    pdf.cell(0, 10, '8-3-22 Branson Tri-Lakes News (49,000 potential reach, Hollister, MO) ' + str(i), 0, 1)
+for i in state_placements:
+    pdf.cell(0, 10, f'{i["date"]} {i["source"]} ({i["reach"]} potential reach)' , 0, 1)
 pdf.set_font('Times', 'B', 12)
 pdf.cell(0, 10, "State Radio Placements", 0, 1)
 pdf.set_font('Times', '', 12)
